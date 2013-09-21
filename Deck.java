e60d2a9bb89e7f737028232455790be3a80b49e6
Collen-Roller <collen.roller@gmail.com>
2013-09-21 13:10:08 -0400

    Deck.java is working

--- Deck.java
+++ Deck.java
@@ -3,57 +3,63 @@ import java.util.*;
 public class Deck {
 
 	private ArrayList<String> deck;
-	private String o1 = "123456789";
+	private String o1 = "23456789";
 	private String o2 = "JQKA";
 
-	//builds a new deck
+	//builds init new deck
 	public Deck() {
 		 deck = new ArrayList<String>();
-
-		 //Adding Hearts to Deck
-		 for(int i=0; i<o1.size(); i++)
+		 newDeck();
+	}
+	
+	//fills array deck with 52 cards
+	public void newDeck(){
+		
+		//Adding Hearts to Deck
+		for(int i=0; i<o1.length(); i++)
 		 	deck.add(o1.charAt(i)+"H");
 		 deck.add("10H");
-		 for(int i=0; i<o2.size(); i++)
+		for(int i=0; i<o2.length(); i++)
 		 	deck.add(o2.charAt(i)+"H");
 
-		 //Adding Spades to Deck
-		 for(int i=0; i<o1.size(); i++)
+		//Adding Spades to Deck
+		for(int i=0; i<o1.length(); i++)
 		 	deck.add(o1.charAt(i)+"S");
-		 deck.add("10S");
-		 for(int i=0; i<o2.size(); i++)
+		deck.add("10S");
+		for(int i=0; i<o2.length(); i++)
 		 	deck.add(o2.charAt(i)+"S");
 		 
-		 //Adding Dimonds to Deck
-		 for(int i=0; i<o1.size(); i++)
+		//Adding Dimonds to Deck
+		for(int i=0; i<o1.length(); i++)
 		 	deck.add(o1.charAt(i)+"D");
-		 deck.add("10D");
-		 for(int i=0; i<o2.size(); i++)
+		deck.add("10D");
+		for(int i=0; i<o2.length(); i++)
 		 	deck.add(o2.charAt(i)+"D");
 		 
-		 //Adding Clubs to Deck
-		 for(int i=0; i<o1.size(); i++)
+		//Adding Clubs to Deck
+		for(int i=0; i<o1.length(); i++)
 		 	deck.add(o1.charAt(i)+"C");
-		 deck.add("10C");
-		 for(int i=0; i<o2.size(); i++)
+		deck.add("10C");
+		for(int i=0; i<o2.length(); i++)
 		 	deck.add(o2.charAt(i)+"C");
-		 }
-
-		 //shuffle deck
-		 public void shuffle(){
-		 	ArrayList<String> temp = new ArrayList<String>();
-		 	for(i=deck.size(); i>=0; i--){
-		 		Random random = new Random();
-		 		int r = random.nextInt(i);
-		 		temp.add(deck.get(chatAt(r)));
-		 		deck.remove(r);
-		 	}
-		 	deck = temp;
-		 }
-
-		 //acessor method for deck
-		public ArrayList<String> newdeck(){
-		 	return deck;
+		
+		//Shuffle Deck after cards are added
+		Collections.shuffle(deck);
+	}
+		
+	//Returns: Random card in the deck
+	//If the deck is empty, a new deck is constructed
+	public String randomCard(){
+		Random rand = new Random();
+		int r = rand.nextInt(deck.size());
+		while(deck.get(r) == null){
+			if (deck.size() == 0)
+				newDeck();
+			else
+				r = rand.nextInt(deck.size());
 		}
+		String s = deck.get(r);
+		deck.remove(r);
+		return s;
 	}
 }
