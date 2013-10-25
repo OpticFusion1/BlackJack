d4d6be50b9a735b7bdb5055d7c78d11a14ce69a6
Collen-Roller <collen.roller@gmail.com>
2013-10-24 23:19:55 -0400

    Added wins, losses, and ties mutator methors/ accessor methods for player statistics

--- Player.java
+++ Player.java
@@ -5,16 +5,45 @@ public class Player {
 	private String name;
 	private boolean isPlaying;
 	private ArrayList<String> cards;
+	private int wins;
+	private int losses;
+	private int ties;
 
 	public Player(boolean b, String s){
 		this.name = s;
 		this.isPlaying = b;
+		this.wins = 0;
+		this.losses = 0;
+		this.ties = 0;
 		this.cards = new ArrayList<String>();
 	}
 
+	//Mutator Methods for Player Class
 	public void addCard(String c){
-		cards.add(c);
+		cards.add(c); //Add a card to players hand
 	}
+
+	public void addWin(){
+		wins++; //Player one a match
+	}
+
+	public void addLoss(){
+		losses++; //Player lost a match
+	}
+
+	public void addTie(){
+		ties++; //Player tied a match
+	}
+
+	public void clear(){
+		cards.clear(); //Clear the players cards
+	}
+
+	public void stillPlaying(boolean b){
+		isPlaying = b; //changes if the player is done playing for not
+	}
+
+	//Accessor Methods for Player Class
 	public ArrayList<String> getCards(){
 		return cards;
 	}
@@ -22,24 +51,22 @@ public class Player {
 	public String getCard(int n){
 		return cards.get(n);
 	}
+
+	public int wins(){
+		return wins;
+	}
+	public int losses(){
+		return losses;
+	}
+	public int ties(){
+		return ties;
+	}
 	
 	public String getName(){
 		return name;
 	}
 	public int getSize(){
 		return cards.size();
-	}
-
-	public void clear(){
-		cards.clear();
-	}
-
-	public void reset(){
-		cards.clear();
-	}
-
-	public void looser(boolean b){
-		isPlaying = b;
 	}
 
 	public boolean isPlaying(){
